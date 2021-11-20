a542a9b826367dfb216f37fb224608c251fae982
KOBULJ0N <qobuljonrasuljonov@gmail.com>
2021-11-20 19:39:16 +0500

    test

--- masala.js
+++ masala.js
@@ -2,7 +2,6 @@
 
 // obj = (num1 = 0, num2 = 0) => {
 //   if (num2) console.log(num1 + num2);
-
 //   return function (num3 = 0) {
 //     console.log(num1 + num3);
 //   };
@@ -31,3 +30,55 @@
 // };
 
 // obj1.getA().getB().getC();
+
+
+
+// var ar = ['apple', 'banana', 'orange', 'apple', 'orange', 'banana']
+// var sorted = ar.sort((x, y) => (x.localeCompare(y)))
+// var res =[]
+// for (i = 0; i < sorted.length; i += 2) {
+//   res.push(sorted[i] )
+// }
+// console.log(res);
+
+// 12 > 10 && console.log('togri');
+
+
+// var number = [10, 5, 11,  2, 3, 4, 5, 6];
+// count = 0;
+// for (i = 0; i < number.length; i++) {
+//   if(typeof number[i] === typeof 3) count += number[i]
+
+// } console.log(count);
+
+
+
+
+
+
+
+
+
+arr = [ '12 ', 'elbek', 'qobiljon', true];
+number = [4, 6, 13, 16, 8, 21,  2, 1];
+
+// num = number.sort((x, y) => (y-x))
+// ar = ['Elbek']
+
+
+// console.log((ar).repeat(5));
+// unshift() push()
+// arr.shift()
+// arr.pop()
+// CRUD 
+// create read update delete
+// arr2 = [1, 2, 3, 4, 5];
+// arr3 = arr.concat(arr2, '12 hello')
+ 
+
+// console.log(number.join())
+
+// console.log((arr3))
+// console.log(arr3);
+// newArr = arr.length
+// console.log(newArr);
