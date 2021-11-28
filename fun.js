309b80b076cbd274c47d6c10ce69ef163ada9800
KOBULJ0N <qobuljonrasuljonov@gmail.com>
2021-11-28 22:42:33 +0500

    masala

---
+++ fun.js
@@ -0,0 +1,73 @@
+// let Status = 'Azo'
+// console.log(result = Status === 'Azo' ? 'chegirmangiz 20%' : 'chegirmangiz 5%');
+
+// let hour =9;
+
+// if (hour >= 6 && hour < 12) {
+//     console.log('Hayrli Tong. ');
+// } else if (hour >= 12 && hour < 18) {
+//     console.log('Hayrli Kun. ');
+// } else {
+//     console.log('Hayrli Kech');
+// }
+
+// str = ''
+// for (let i = 0; i < 7; i++) {
+//     console.log(str += '*', );
+// }
+
+// count = ' '
+// for (i = 0; i < 7; i++) {
+//  console.log( count+=  '*'  )
+// }
+
+// count = ' ';
+// for (j = 0; j < 5; j++) {
+
+//     for (i = 0; i < 5; i++) {
+//         console.log((count += ''));
+//     }
+//      console.log((count += '*'));
+// }
+
+/* // function fizzBuzz(input) {
+//   if (typeof input !== 'number') return 'Bu son emass !';
+//   else if (input % 3 === 0 && input % 5 === 0) return 'fizBuzz';
+//   else if (input % 3 === 0) return 'fizz';
+//   else if (input % 5 === 0) return 'Buzz';
+//   else return input;
+// }
+
+// console.log(fizzBuzz(16));
+
+
+*/
+// Tezlik cheklovi 70km/soat
+// 70+  5 km ==> 1 point
+// 10 point ==> Guvohnoma olib qoyilsin
+
+// const SPEED_LIMIT = 70; // km/soat
+// const KM_PER_POINT = 5; // km/soat
+// checkSpeed(90);
+
+// function checkSpeed(speed) {
+//     if (speed < SPEED_LIMIT +KM_PER_POINT) console.log('OK');
+//     else {
+//         let point = Math.floor((speed - SPEED_LIMIT) / KM_PER_POINT);
+//         if (point >= 10) {
+//             console.log('Guvohnoma Olib qoyildi');
+//         } else
+//             console.log(( 10 -`${point}`) + ' point dan keyin Guvohnoma olinadi. Jarimalar soni :', `${point} ta`, );
+//     }
+// }
+
+// str = ' WEB Academy';
+// str1 = ''
+// // str1 = str.repeat(10);
+// // var str2;
+// for (i = 0; i < 10; i++) {
+//   str1 += str.replace('WEB', 'IT');
+//   // console.log(  str2 = str1.replace('WEB', 'IT'));
+// }
+
+// console.log(str1);
--- masala.js
+++ masala.js
@@ -32,53 +32,49 @@
 // obj1.getA().getB().getC();
 
 
-
-// var ar = ['apple', 'banana', 'orange', 'apple', 'orange', 'banana']
-// var sorted = ar.sort((x, y) => (x.localeCompare(y)))
-// var res =[]
-// for (i = 0; i < sorted.length; i += 2) {
-//   res.push(sorted[i] )
-// }
-// console.log(res);
-
-// 12 > 10 && console.log('togri');
-
-
-// var number = [10, 5, 11,  2, 3, 4, 5, 6];
 // count = 0;
-// for (i = 0; i < number.length; i++) {
-//   if(typeof number[i] === typeof 3) count += number[i]
+// res = 0;
 
-// } console.log(count);
+// const sum = (arr, num) => {
+//   for (i = 0; i < arr.length; i++) {
+//     (count += arr[i]) / (i + 1) == num && res++;
+
+//     qosh = 0;
+//     f = 1
+
+//     for (j = i + 1; j < arr.length; j++) {
+//       (qosh += arr[j]) / f == num && res++;
+//       f++;
+//     }
+//   }
+//   return res;
+// };
+// console.log(sum([1, 3, 2, 2], 2));
+
+
+
+// var a = 'Ali Ubaydullayev';
+
+// str = a.split('') 
+
+// // console.log(str);
+// for (i = 0; i < str.length; i++) {
+//     if (str[i] === str[i].toUpperCase() && str[i] !== ' ' ) {
+//           console.log(str[i]);
+//       }
+//     }
 
 
 
 
+function sum(limit) {
+    counter = 0;
+    
+    for (i = 0; i < limit.length; i++) {
+        if (limit % ==)
+    }
+        
+}
 
+console.log(sum(10));
 
-
-
-
-arr = [ '12 ', 'elbek', 'qobiljon', true];
-number = [4, 6, 13, 16, 8, 21,  2, 1];
-
-// num = number.sort((x, y) => (y-x))
-// ar = ['Elbek']
-
-
-// console.log((ar).repeat(5));
-// unshift() push()
-// arr.shift()
-// arr.pop()
-// CRUD 
-// create read update delete
-// arr2 = [1, 2, 3, 4, 5];
-// arr3 = arr.concat(arr2, '12 hello')
- 
-
-// console.log(number.join())
-
-// console.log((arr3))
-// console.log(arr3);
-// newArr = arr.length
-// console.log(newArr);
