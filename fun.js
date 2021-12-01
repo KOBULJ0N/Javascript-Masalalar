d7bde754ffd180a006645b0287a9c61caad4ca6d
KOBULJ0N <qobuljonrasuljonov@gmail.com>
2021-12-01 23:56:53 +0500

    sth new m

--- fun.js
+++ fun.js
@@ -75,14 +75,73 @@
 
 
 // const letterCapital = (string) => {
-//    str = string.split('');
-//    res = '';
-//    for (i = 0; i < str.length; i++) {
-//      if (str[i] === str[i].toUpperCase() && str[i] !== ' ') {
-//        res += str[i];
+//     res = '';
+//    for (i = 0; i < string.length; i++) {
+//      if (string[i] === string[i].toUpperCase() && string[i] !== ' ') {
+//        res += string[i];
 //      }
 //    } return res
 // }
-
-
 // console.log(letterCapital('Webbrain Academy Is Number One'));
+
+
+
+
+
+/*Bosh ogrigi 1 */ 
+
+// obj = (num1 = 0, num2 = 0) => {
+//   if (num2) console.log(num1 + num2);
+//   return function (num3 = 0) {
+//     console.log(num1 + num3);
+//   };
+// };
+
+// obj(1, 2);
+// obj(1)(2);
+
+/*   Boshni ogritgan 2 chi masala */
+// const obj1 = {
+//   a: 1,
+//   b: 2,
+//   c: 3,
+//   getA() {
+//     console.log(this.a);
+//     return this;
+//   },
+
+
+//   getB() {
+//       console.log(this.b);
+//       return this
+//     },
+//     getC() {
+//         console.log(this.c);
+//         return this
+//   }
+// };
+
+// obj1.getA().getB().getC();
+
+
+// count = 0;
+// res = 0;
+
+// const sum = (arr, num) => {
+//   for (i = 0; i < arr.length; i++) {
+//     (count += arr[i]) / (i + 1) == num && res++;
+
+//     qosh = 0;
+//     f = 1
+
+//     for (j = i + 1; j < arr.length; j++) {
+//       (qosh += arr[j]) / f == num && res++;
+//       f++;
+//     }
+//   }
+//   return res;
+// };
+// console.log(sum([1, 3, 2, 2], 2));
+
+
+ 
